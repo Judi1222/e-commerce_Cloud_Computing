2817532a21d4db6135fac0f9b082ea708bbfcd06
Judi <yoditgezaw@gmail.com>
2025-04-28 16:29:42 +0300

    first commit

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,20 @@
+const Product = require('../models/productModel');
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.getAllProducts();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.getProductById(req.params.id);
+    if (!product) return res.status(404).json({ error: 'Product not found' });
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
